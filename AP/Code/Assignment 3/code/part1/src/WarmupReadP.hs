a7c57e3fc79a3c6985c87bbff2b8d6a692445020
Michal Pikulski <michal.a.pikulski@gmail.com>
2019-09-20 08:23:45 +0200

    [Assignment 3] Add TA code

--- AP/Code/Assignment 3/code/part1/src/WarmupReadP.hs
+++ AP/Code/Assignment 3/code/part1/src/WarmupReadP.hs
@@ -3,12 +3,16 @@ module WarmupReadP where
 -- Original grammar (E is start symbol):
 --   E ::= E "+" T | E "-" T | T | "-" T .
 --   T ::= num | "(" E ")" .
+
 -- Lexical specifications:
 --   num is one or more decimal digits (0-9)
 --   tokens may be separated by arbtrary whitespace (spaces, tabs, newlines).
 
 -- Rewritten grammar, without left-recursion:
---   <<< fill in here >>>
+-- Exp   ::= Term Exp' | "-" Term
+-- Exp'  ::= e | Op Term Exp'
+-- Op    ::= "+" | "-"
+-- Term  ::= num | "(" Exp ")"
 
 import Text.ParserCombinators.ReadP
 import Control.Applicative ((<|>))
@@ -22,4 +26,32 @@ data Exp = Num Int | Negate Exp | Add Exp Exp
   deriving (Eq, Show)
 
 parseString :: String -> Either ParseError Exp
-parseString = undefined
+parseString s = 
+    case reverse $ readP_to_S expr s of
+        [] -> Left "Arghhhhhhh"
+        ((exp, _):_) -> Right exp
+
+-- char - parser of Chars from ReadP
+expr :: Parser Exp
+expr = (term >>= expr')
+    <|> (char '-' >> Negate <$> term)
+
+expr' :: Exp-> Parser Exp
+expr' e1 = (char '+' >> Add e1 <$> term >>= expr')
+    <|> (char '-' >> Add e1 <$> (Negate <$> term) >>= expr')
+    <|> (return e1)
+
+-- between from ReadP
+term :: Parser Exp
+term = (Num <$> num)
+    <|> (between (char '(') (char ')') expr)
+
+-- num :: Parser Int
+-- num = read <$> (munch1 (\c -> c `elem` "0123456789))
+-- choice parser
+
+some :: Parser a -> Parser [a]
+some p = (:) <$> p <*> many p
+
+num :: Parser Int
+num = read <$> some (satisfy (\c -> c `elem` "0123456789"))
