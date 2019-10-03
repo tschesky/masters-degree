fa68e75ff0c4d75c55a5d2ee3c8429e6e8b46d8c
Till Grenzdoerffer <tgrenzdoerff@uos.de>
2019-10-03 15:28:07 +0200

    routing + requests WIP, added .beam to gitignore

--- AP/Code/Assignment 5/code/part2/src/flamingo.erl
+++ AP/Code/Assignment 5/code/part2/src/flamingo.erl
@@ -1,28 +1,90 @@
 -module(flamingo).
 
--export([start/1, new_route/3, request/4, drop_route/2]).
--import(maps, [from_list/1,
-			   merge/2]).
+-export([start/1, new_route/3, request/4, drop_route/2, lookup_route/2]).
+-import(lists, [filter/2]).
 
-add_to_env(Env, Prefixes, Action) ->
-	TmpList = [X, Y || X <- Prefixes, Y <- Action],
-	TmpDict = maps:from_list(TmpList),
-	maps:merge(Env, TmpDict).
-
-flamingo_main_function(Env) ->
-	receive
-		{register, Prefixes, Action} -> NewEnv = add_to_env(Env, Prefixes, Action),
-		flamingo_main_function(NewEnv)
+get_response(Routing, Env, {Path, _}=Req) -> 
+	try
+		case lookup_route(Routing, Path) of
+			none -> {404, "text/html", ""};
+			{_,_, Action} -> Action(Req, Env)
+		end
+	catch
+		throw :  e -> {500, "text/html", ""};
+		error : e -> {500, "text/html", ""};
+		exit :   e -> {500, "text/html", ""}
 	end.
 
-start(_Global) ->
-    nope.
 
-request(_Flamingo, _Request, _From, _Ref) ->
-    nope.
+lookup_route(Routing, Path) -> 
+	Candidates = lists:filter(fun({X, _, _}) -> filter_path(Path, X) end, Routing),
+	case Candidates of
+		[] -> none;
+		_  -> get_longest_prefix(Candidates, {"", none, none})
+	end.
+	
 
-new_route(_Flamingo, _Prefixes, _Action) ->
-    nope.
+	
+
+
+
+get_longest_prefix([], Longest) -> Longest;
+get_longest_prefix([{Prefix, _, _}=Head | Tail], {Longest_Prefix, _, _})
+								when (length(Prefix) > length(Longest_Prefix)) -> get_longest_prefix(Tail, Head);
+get_longest_prefix([_ | Tail], Longest) -> get_longest_prefix(Tail, Longest).
+
+
+filter_path(Path, Prefix) -> 	case string:prefix(Path, Prefix) of
+									nomatch -> false;
+									_ -> true
+								end.
+
+
+add_routes(Env, Prefixes, Action) ->
+	try
+		Id = make_ref(),
+		TmpList = [{X, Id, Action} || X <- Prefixes],
+		{Id, TmpList ++ Env}
+	catch
+		throw: e -> {error, e};
+		error: e -> {error, e}
+	end.
+
+
+loop(Routing, Env) ->
+	receive
+		{register, Pid, Prefixes, Action} -> Res = add_routes(Routing, Prefixes, Action), % TODO: error handling
+												case Res of
+													{Id, NewRouting} -> 
+														Pid ! {ok, Id},
+														loop(NewRouting, Env);
+													{error, e} -> 
+														Pid ! {error, e},
+														loop(Routing, Env)
+												end;
+		{request, Req, From, Ref} ->  Res = get_response(Routing, Env, Req),
+									  From ! {Ref, Res}
+	end.
+
+
+
+start(Global) ->
+	try 
+    	{ok, spawn(fun() -> loop([], Global) end)}
+	catch
+		error: e -> {error, e}
+	end.
+
+request(Flamingo, Request, From, Ref) ->
+    Flamingo ! {request, Request, From, Ref}.
+
+new_route(Flamingo, Prefixes, Action) ->
+    Flamingo ! {register, self(), Prefixes, Action},
+	receive
+		{ok, Id} 		->	{ok, Id};
+		{error, Reason} -> 	{error, Reason}
+	end.
+		
 
 drop_route(_Flamingo, _Id) ->
     not_implemented.
