a5a5d31753f27c2cb7047fd92530962a9d43c1c5
Michal Pikulski <michal.a.pikulski@gmail.com>
2019-10-03 17:20:52 +0200

    [Assignment 5] Initial hello version

--- AP/Code/Assignment 5/code/part2/src/hello.erl
+++ AP/Code/Assignment 5/code/part2/src/hello.erl
@@ -1,4 +1,22 @@
 -module(hello).
--export([server/0]).
+-export([server/0, try_it/2]).
 
-server() -> nope.
+hello(_, _) ->
+	{200, "text/plain", "Hello my dear friend"}.
+
+goodbye(_, _) ->
+	{200, "text/plain", "Sad to see you go already."}.
+
+server() ->
+	{ok, F} = flamingo:start("The Falmingo Server"),
+	flamingo:new_route(F, ["/hello"], fun hello/2),
+	flamingo:new_route(F, ["/goodbye"], fun goodbye/2),
+	F.
+
+try_it(Server, Path) ->
+	Ref = make_ref(),
+	flamingo:request(Server, {Path, []}, self(), Ref),
+	receive
+        {Ref, Reply} -> Reply
+    end.
+
