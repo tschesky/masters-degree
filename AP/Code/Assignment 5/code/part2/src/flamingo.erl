4f9ccbc29309fad655fff9c72f88650dda3159e3
Michal Pikulski <michal.a.pikulski@gmail.com>
2019-10-07 15:02:15 +0200

    Work in progress

--- AP/Code/Assignment 5/code/part2/src/flamingo.erl
+++ AP/Code/Assignment 5/code/part2/src/flamingo.erl
@@ -1,13 +1,21 @@
 -module(flamingo).
 
 -export([start/1, new_route/3, request/4, drop_route/2, lookup_route/2]).
--import(lists, [filter/2]).
+-import(lists, [filter/2, foldl/3]).
 
 get_response(Routing, Env, {Path, _}=Req) -> 
 	try
 		case lookup_route(Routing, Path) of
 			none -> {404, "text/html", ""};
-			{_,_, Action} -> Action(Req, Env)
+			{_,_, Action} -> 
+				% Quick and dirty - but we need to do sth like that here anyway, there was a discussion
+				% On absalon that we should convert stupid output from Actions into reasonable response format
+				case Action(Req, Env) of
+				 	{200, _, _}=Resp -> Resp;
+				 	{404, _, _}=Resp -> Resp;
+				 	{500, _, _}=Resp -> Resp;
+				 	_ -> {500, "text/html", ""}
+				end
 		end
 	catch
 		throw :  e -> {500, "text/html", ""};
@@ -16,22 +24,19 @@ get_response(Routing, Env, {Path, _}=Req) ->
 	end.
 
 lookup_route(Routing, Path) -> 
-	Candidates = lists:filter(fun({X, _, _}) -> filter_path(Path, X) end, Routing),
-	case Candidates of
-		[] -> none;
-		_  -> get_longest_prefix(Candidates, {"", none, none})
+	Route = lists:foldl(fun(X, Longest) -> filter_path(Path, X, Longest) end, "", Routing),
+	case Route of
+		"" -> none;
+		_ -> Route
 	end.
-	
-get_longest_prefix([], Longest) -> Longest;
-get_longest_prefix([{Prefix, _, _}=Head | Tail], {Longest_Prefix, _, _})
-								when (length(Prefix) > length(Longest_Prefix)) -> get_longest_prefix(Tail, Head);
-get_longest_prefix([_ | Tail], Longest) -> get_longest_prefix(Tail, Longest).
 
-
-filter_path(Path, Prefix) -> case string:prefix(Path, Prefix) of
-									nomatch -> false;
-									_ -> true
-								end.
+filter_path(Path, {X, _, _}, Longest) -> case string:prefix(X, Path) of
+								         	  nomatch -> Longest;
+								        	  _ -> case (length(X) > length(Longest)) of
+								        		 	  true -> X;
+								        		 	  false -> Longest
+								          		   end
+								          end.
 
 add_routes(Routing, Prefixes, Action) ->
 	try
@@ -40,20 +45,21 @@ add_routes(Routing, Prefixes, Action) ->
 		{ok, Id, TmpList ++ Routing}
 	catch
 		throw: e -> {error, e};
+		exit: e -> {error, e};
 		error: e -> {error, e}
 	end.
 
 loop(Routing, Env) ->
 	receive
-		{register, Pid, Prefixes, Action} -> Res = add_routes(Routing, Prefixes, Action), % TODO: error handling
-												case Res of
-													{ok, Id, NewRouting} -> 
-														Pid ! {ok, Id},
-														loop(NewRouting, Env);
-													{error, e} -> 
-														Pid ! {error, e},
-														loop(Routing, Env)
-												end;
+		{register, Pid, Prefixes, Action} -> Res =  add_routes(Routing, Prefixes, Action), % TODO: error handling
+											 case Res of
+											 	{ok, Id, NewRouting} -> 
+											 		Pid ! {ok, Id},
+											 		loop(NewRouting, Env);
+											 	{error, e} -> 
+											 		Pid ! {error, e},
+											 		loop(Routing, Env)
+											 end;
 		{request, Req, From, Ref} ->  Res = get_response(Routing, Env, Req),
 									  From ! {Ref, Res},
 									  loop(Routing, Env)
@@ -73,7 +79,8 @@ new_route(Flamingo, Prefixes, Action) ->
     Flamingo ! {register, self(), Prefixes, Action},
 	receive
 		{ok, Id} 		->	{ok, Id};
-		{error, Reason} -> 	{error, Reason}
+		{error, Reason} -> 	{error, Reason};
+		_ -> blah
 	end.
 		
 drop_route(_Flamingo, _Id) ->
